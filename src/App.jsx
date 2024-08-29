91b0f3a1cb30e091258649a4cafe14a37b33a284
Satriyo Rizkyansah <satttrioo@gmail.Com>
2024-08-29 16:34:17 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-200 min-h-screen items-center text-white">
+      <div>Hello World</div>
+    </div>
+  );
+}
+
+export default App;
