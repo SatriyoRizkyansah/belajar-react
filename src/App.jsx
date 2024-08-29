e7b868f4c7ad5c0b8218a4c8dc4528c3a9ac6c7c
Satriyo Rizkyansah <satttrioo@gmail.Com>
2024-08-29 19:10:45 +0700

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import React from "react";
+import { renderToReadableStream } from "react-dom/server";
+
+const Button = (props) => {
+  const { children = "...", variant = "black" } = props;
+  return (
+    <button type="button" className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}>
+      {props.children}
+    </button>
+  );
+};
+
 function App() {
   return (
     <div className="flex justify-center bg-blue-200 min-h-screen items-center text-white">
+      div
       <div>Hello World</div>
+      <Button variant="bg-red-700">Login</Button>
+      <Button variant="bg-slate-700">Logout</Button>
+      <Button>Logout</Button>
     </div>
   );
 }
