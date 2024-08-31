d9e2607ac343abc234286fe204280d1e1369a5e4
Satriyo Rizkyansah <satttrioo@gmail.Com>
2024-08-31 20:46:36 +0700

    learning conditional rendering

--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -1,17 +1,10 @@
 import AuthLayouts from "../components/Layouts/AuthLayouts";
 import FormRegister from "../components/Fragments/FormRegister";
-import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
-    <AuthLayouts title="Register">
+    <AuthLayouts title="Register" type="register">
       <FormRegister></FormRegister>
-      <p className="text-sm text-neutral-900 my-4">
-        Already have an account? Login{" "}
-        <Link to="/login" className="text-blue-600 hover:underline">
-          here
-        </Link>
-      </p>
     </AuthLayouts>
   );
 };
--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,13 +1,39 @@
 import { Children } from "react";
+import { Link } from "react-router-dom";
 
 const AuthLayouts = (props) => {
-  const { children, title } = props;
+  const { children, title, type } = props;
   return (
     <div className="flex justify-center min-h-screen items-center text-white">
       <div className="w-full max-w-xs">
         <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
         <p className="font-medium text-slate-500 mb-8">Welcome, Please enter your details</p>
         {children}
+        <p className="text-sm text-neutral-900 my-4">
+          {type === "login" ? "Dont have an Account? " : "Already have an account? "}
+
+          {type === "login" ? (
+            <Link to="/register" className="text-blue-600 font-medium hover:underline">
+              Login
+            </Link>
+          ) : (
+            <Link to="/login" className="text-blue-600 font-medium hover:underline">
+              Register
+            </Link>
+          )}
+
+          {/* {type === "login" && (
+            <Link to="/register" className="text-blue-600 font-medium hover:underline">
+              Register
+            </Link>
+          )}
+
+          {type === "register" && (
+            <Link to="/login" className="text-blue-600 font-medium hover:underline">
+              Login
+            </Link>
+          )} */}
+        </p>
       </div>
     </div>
   );
