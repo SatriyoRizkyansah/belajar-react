9b6efbd409751cf510750b36cfc5338d3db6ae4a
Satriyo Rizkyansah <satttrioo@gmail.Com>
2024-09-01 12:33:30 +0700

    learning about Rendering List

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,18 +1,32 @@
 import CardProduct from "../components/Fragments/CardProducts";
+
+const products = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    price: "Rp 1.000.000",
+    image: "/images/shoes-1.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quae obcaecati officia commodi accusamus vitae aspernatur autem qui illo in.",
+  },
+  {
+    id: 2,
+    name: "Sepatu Lama",
+    price: "Rp 2.000.000",
+    image: "/images/shoes-1.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quae",
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-10">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg"></CardProduct.Header>
-        <CardProduct.Body title="Sepatu Baru">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quae obcaecati officia commodi accusamus vitae aspernatur autem qui illo in.</CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000"></CardProduct.Footer>
-      </CardProduct>
-
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg"></CardProduct.Header>
-        <CardProduct.Body title="Sepatu Baru">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi quae obcaecati officia commodi accusamus vitae aspernatur autem qui illo in.</CardProduct.Body>
-        <CardProduct.Footer price="Rp 1.000.000"></CardProduct.Footer>
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct>
+          <CardProduct.Header image={product.image}></CardProduct.Header>
+          <CardProduct.Body name={product.name}>{product.description}</CardProduct.Body>
+          <CardProduct.Footer price={product.price}></CardProduct.Footer>
+        </CardProduct>
+      ))}
     </div>
   );
 };
